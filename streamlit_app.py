b2cc9817aaba5a155ef6bb86af5d825ecba2a05a
Robert Guerra <59858155+RobertJG17@users.noreply.github.com>
2022-09-25 23:56:43 -0700

    Update streamlit_app.py
    removed unnecessary display text
    made requests url modular to allow for variable interpolation
    added in code to make api request response a bit more visually appealing

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,5 +25,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
+
+# Flatten nested json
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Display normalized json in a table view
+streamlit.dataframe(fruityvice_normalized)
