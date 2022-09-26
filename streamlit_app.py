713d3abc5014a9c8eeffc241bc34be8a5c1a6737
Robert Guerra <59858155+RobertJG17@users.noreply.github.com>
2022-09-26 00:00:15 -0700

    Update streamlit_app.py
    created dynamic table by interpolating the fruit_choice variable

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # Flatten nested json
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
