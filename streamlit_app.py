d43932c8f64f1a3dd5b681c2ed680b97fd767bb8
Robert Guerra <59858155+RobertJG17@users.noreply.github.com>
2022-09-27 22:43:15 -0700

    Update streamlit_app.py
    created try except block with nested if
    removed unnecessary code/comments

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,16 +26,21 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
-# Send an API request to Fruityvice API
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# Flatten nested json
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Display normalized json in a table view
-streamlit.dataframe(fruityvice_normalized)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    # Send an API request to Fruityvice API
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    # Flatten nested json
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    # Display normalized json in a table view
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
+  
 
 # ---- Don't run anything past here while we troubleshoot ----
 streamlit.stop()
