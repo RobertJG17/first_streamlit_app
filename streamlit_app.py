96f5d5cf58ab04766ae78045ffe6712ccf93db4f
Robert Guerra <59858155+RobertJG17@users.noreply.github.com>
2022-09-27 23:22:55 -0700

    Update streamlit_app.py
    added logic to properly insert fruits upon button click

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,11 +56,18 @@ if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
-streamlit.stop()
 
-# Text input box for adding fruits to fruit list
+# Allow the end user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute(f"insert into fruit_load_list values ('{new_fruit}')")
+    return "Thanks for adding " + new_fruit
+  
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thank you for adding ', add_my_fruit)
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.write(back_from_function)
 
 # This will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
