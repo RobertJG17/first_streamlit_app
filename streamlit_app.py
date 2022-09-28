80ff16f1c2f09f4b0f222b8279384b194b95a5bc
Robert Guerra <59858155+RobertJG17@users.noreply.github.com>
2022-09-27 23:27:21 -0700

    Update streamlit_app.py
    added code to terminate snowflake connection after running queries

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,6 +55,7 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
+  my_cnx.close()
   streamlit.dataframe(my_data_rows)
 
 # Allow the end user to add a fruit to the list
@@ -67,6 +68,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
+  my_cnx.close()
   streamlit.write(back_from_function)
 
 # This will not work correctly, but just go with it for now
